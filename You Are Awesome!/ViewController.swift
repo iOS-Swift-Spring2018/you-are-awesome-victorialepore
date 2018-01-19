b5e55f8a4a69eb6be904fc408228b4f89eb35c13
Victoria LePore <Tori@MacBook-Pro.local>
2018-01-18 23:29:57 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Victoria LePore on 1/18/18.
+//  Copyright © 2018 Victoria LePore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
